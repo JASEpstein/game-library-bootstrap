f0f36fb4395257fcce7a42c8687f4fae2a60429a
Jamie Epstein <jasepstein@gmail.com>
2020-12-30 18:33:10 -0600

    Server is set up with OpenID auth via Okta

--- game-library/app.js
+++ game-library/app.js
@@ -1,7 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
+var session = require('express-session');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -16,8 +16,13 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: 'opajeohpowuehpofajsofjpw',
+  resave: true,
+  saveUninitialized: false
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
